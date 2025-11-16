8a0061e0313255d01b00b5d12c5fa7ce4272f75c
jakhongir <irismatoffj@mail.ru>
2025-11-16 21:45:44 +0500

    last

--- service-registry/src/main/java/com/malaka/aat/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/malaka/aat/serviceregistry/ServiceRegistryApplication.java
@@ -3,7 +3,6 @@ package com.malaka.aat.serviceregistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
